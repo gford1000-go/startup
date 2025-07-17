59f0fd2f6826add9471a14d8e7fa501825fad081
Geoff Ford <geoff.golang.12@gmail.com>
2025-07-17 22:31:15 +0100

    Changed variable names

--- identity_test.go
+++ identity_test.go
@@ -16,7 +16,7 @@ func ExampleCreateAndRegisterID() {
 
 	thisIsBob := func(ctx context.Context, opts *FunctionOptions) {
 		// Initialise Bob to just reflect back what it was given
-		id, err := CreateAndRegisterID(opts.DiscoveryService, bobID, time.Minute, func(ctx context.Context, r1 *Req, r2 *Res) {
+		bob, err := CreateAndRegisterID(opts.DiscoveryService, bobID, time.Minute, func(ctx context.Context, r1 *Req, r2 *Res) {
 			r2.Type = r1.Type
 			r2.Data = r1.Data
 			r2.Status = Success
@@ -26,18 +26,18 @@ func ExampleCreateAndRegisterID() {
 		}
 
 		// Bob now waits for Connection requests and handles them
-		id.Accept(ctx)
+		bob.Accept(ctx)
 	}
 
 	thisIsAlice := func(ctx context.Context, opts *FunctionOptions) {
 
 		// Alice is only initiating requests, not handling them
-		id, err := CreateAndRegisterID(opts.DiscoveryService, aliceID, time.Minute, nil)
+		alice, err := CreateAndRegisterID(opts.DiscoveryService, aliceID, time.Minute, nil)
 		if err != nil {
 			panic(err)
 		}
 
-		c, err := id.Connect(ctx, bobID, WithConnectDiscoveryService(opts.DiscoveryService))
+		c, err := alice.Connect(ctx, bobID, WithConnectDiscoveryService(opts.DiscoveryService))
 		if err != nil {
 			panic(err)
 		}
@@ -47,7 +47,7 @@ func ExampleCreateAndRegisterID() {
 			Data: []byte("Hello World"),
 		}
 
-		r := id.Send(ctx, req, c.ReqChan)
+		r := alice.Send(ctx, req, c.ReqChan)
 
 		// Reflection should be successful
 		fmt.Println(r.Status == Success ||
